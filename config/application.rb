d1bf2aa84c3e91a92cc8d9e232428ebd3d907971
Britt Ballard <davidbritton.ballard@gmail.com>
2017-03-22 17:18:30 -0500

    What about now?

--- config/application.rb
+++ config/application.rb
@@ -57,5 +57,6 @@ module LastHopeCartel
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.eager_load = true
   end
 end
